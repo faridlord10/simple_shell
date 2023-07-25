7b731907d1c53e7761b42e6e8d484933be5445f1
faridlord10 <fahizoune10@gmail.com>
2023-07-25 15:36:44 -0700

    Get environnement

---
+++ get_env.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * get_env - get the path of the environnement.
+ * @env: global var.
+ * @pathname: directory of environ.
+ * Return: the path or null.
+ */
+char *get_env(char **env, char *pathname)
+{
+	char *txt, *path;
+	int i, j;
+
+	for (i = 0; env[i]; i++)
+	{
+		txt = malloc(1024);
+
+		for (j = 0; env[i][j] != '='; j++)
+			txt[j] = env[i][j];
+
+		if (comp_str(txt, pathname))
+		{
+			path = length_str(env[i]);
+			free(txt);
+			return (path);
+		}
+		free(txt);
+	}
+
+	return (NULL);
+}
+
